90bcdc5d1399f6ccdd4b01b013e3c23ee75e2855
Keimah507 <githinjibrian5@gmail.com>
2022-12-21 21:29:38 +0000

    Task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,16 +1,4 @@
 function getResponseFromAPI () {
-  const i = 5;
-  const promise = new Promise((resolve, reject) => {
-    if (i === 5) {
-      resolve('Successful call');
-    } else {
-      reject(err);
-    }
-  });
-  promise.then((message) => {
-    console.log(message);
-  });
+  return new Promise(() => {});
 }
 
-const response = getResponseFromAPI();
-console.log(response instanceof Promise);
