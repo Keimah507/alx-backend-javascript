fd43e7ad54e76343a6ac67e8327b10504f0fdb6a
keimah507 <githinjibrian5@gmail.com>
2022-12-22 02:02:07 +0300

    Lint fixes

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,17 +1,17 @@
-import {uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils.js';
 
-export default async function asyncUploadUser() {
-	try {
-		const photo = await uploadPhoto();
-		const user = await CreateUser();
-		return {
-			photo,
-			user,
-		};
-	} catch (error) {
-		return {
-			photo: null,
-			user: null,
-		};
-	}
+export default async function asyncUploadUser () {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return {
+      photo,
+      user,
+    };
+  } catch (error) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
 }
--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
-import {uploadPhoto, createUser} from './utils.js';
+import { uploadPhoto, createUser } from './utils.js';
 
-export default function handleProfileSignup() {
-	return Promise.all([uploadPhoto(), createUser()])
-	.then((response) => {
-		const { body } = response[0];
-		const {firstName, lastName } = response[1];
-		console.log(`${body} ${firstName} ${lastName}`);
-	})
-	.catch(() => console.log('Signup system offline'));
+export default function handleProfileSignup () {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((response) => {
+      const { body } = response[0];
+      const { firstName, lastName } = response[1];
+      console.log(`${body} ${firstName} ${lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
