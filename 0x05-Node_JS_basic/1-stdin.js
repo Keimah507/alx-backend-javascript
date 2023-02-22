90d1616208488cd86fb91835d6d8ba7cc21e4c04
keimah507 <githinjibrian5@gmail.com>
2023-02-22 05:37:31 +0300

    Refactor code

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 
-const readline = require('readline').createInterface({
-	input: process.stdin,
-	output: process.stdout,
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) process.stdout.write(`Your name is: ${name}`);
+});
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
 
-readline.question(`Welcome to Holberton School, what is your name?`, name => {
-	const answer = await readline,question(`Your name is; ${name}`);
-
-	console.log('This important software is now closing');
-
-	readline.close();
-});
