a35ef15ca8fecb444f0cf32ffd9e7890c475235f
keimah507 <githinjibrian5@gmail.com>
2023-02-24 13:23:22 +0300

    Task 0

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -2,10 +2,22 @@
 const calculateNumber = require("./1-calcul");
 const assert = require("assert");
 
-describe("calculateNumber type == SUM", function(){
-    it("checks various operations on rounded numbers", function() {
-        assert.equal(calculateNumber('SUM', 1.7, 2.5), 4);
-        assert.equal(calculateNumber('SUM', 3.2, 3), 0);
-        assert.equal(calculateNumber('SUM', 2.2, 3), 1);
+describe("calculateNumber", function(){
+    it("checks sum operation on rounded numbers", function() {
+        assert.equal(calculateNumber('SUM', 1.7, 2.5), 5);
+        assert.equal(calculateNumber('SUM', 3.2, 3), 6);
+        assert.equal(calculateNumber('SUM', 2.2, 3), 5);
+    });
+
+    it("Checks subtract operation on rounded numbers", function() {
+        assert.equal(calculateNumber('SUBTRACT', 3, 5), -2);
+        assert.equal(calculateNumber('SUBTRACT', 4.5, 3), 2);
+        assert.equal(calculateNumber('SUBTRACT', -2.7, -4.6), 2);
+    });
+
+    it("Checks Divide operation on rounded numbers", function() {
+        assert.equal(calculateNumber('DIVIDE', 4, 2), 2);
+        assert.equal(calculateNumber('DIVIDE', 9.5, 4.5), 2);
+        assert.equal(calculateNumber('DIVIDE', 8.7, 2.5), 3);
     });
 });
