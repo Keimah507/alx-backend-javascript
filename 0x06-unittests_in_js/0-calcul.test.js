1b238b00423dd30e73e46eed1f1eb8cf1c761435
keimah507 <githinjibrian5@gmail.com>
2023-02-25 00:43:03 +0300

    Task 0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -8,4 +8,15 @@ describe("calculateNumber", function() {
         assert.strictEqual(calculateNumber(1, 3.7), 5);
         assert.strictEqual(calculateNumber(1.5, 3.7), 6)
     });
+
+    it("Tests summation of negative numbers", function() {
+        assert.strictEqual(calculateNumber(-1, 1), 0);
+        assert.strictEqual(calculateNumber(1, -1), 0);
+        assert.strictEqual(calculateNumber(-1, -1), -2);
+    });
+
+    it("Checks arguments", function() {
+        assert.strictEqual(isNaN(calculateNumber(1)), true);
+        assert.strictEqual(isNaN(calculateNumber()), true);
+    });
 });
